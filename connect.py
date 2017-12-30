455046632de5a10e7cbc5c62b96e4b503725a52b
JBBgameich <jbbgameich@gmail.com>
2017-12-30 15:41:57 +0100

    Add initial python rewrite of the connect script, untested

---
+++ connect.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+import re
+import subprocess
+import sys
+import argparse
+
+def get_rndis_device():
+    rndis_device = subprocess.check_output('sudo dmesg | grep rndis | grep -oh "\w*enp0\w*" | tail -n 1', shell=True).decode()
+
+    if not rndis_device == "":
+        print("* The device seems to be connected as " + rndis_device)
+        return rndis_device
+    else:
+        print("Is the device connected?")
+        sys.exit(1)
+
+def connect_ssh(rndis_device):
+    ssh_local_ip = "10.15.19.100"
+    ssh_ip = "10.15.19.82"
+
+    subprocess.call(["sudo", "ip", "address", "add", ssh_local_ip, "dev", rndis_device])
+    subprocess.call(["sudo", "ip", "route", "add", ssh_ip, "dev", rndis_device])
+
+    print("* Connecting using ssh")
+    subprocess.call(["ssh", args.username + "@" + ssh_ip])
+
+def connect_telnet(rndis_device):
+    telnet_local_ip = "192.168.2.1"
+    telnet_ip = "192.168.2.15"
+
+    subprocess.call(["sudo", "ip", "address", "add", telnet_local_ip, "dev", rndis_device])
+    subprocess.call(["sudo", "ip", "route", "add", telnet_ip, "dev", rndis_device])
+
+    print("* Connecting using telnet")
+    subprocess.call(["telnet", telnet_ip])
+
+
+parser = argparse.ArgumentParser(description="Connect to Halium powered devices")
+parser.add_argument("-p", "--protocol", help="Protocol to use for connecting to the device")
+parser.add_argument("-u", "--username", help="Username to use. Usually root or phablet")
+
+args = parser.parse_args()
+
+if args.protocol == "ssh":
+    connect_ssh(get_rndis_device())
+elif args.protocol == "telnet":
+    connect_telnet(get_rndis_device())
+elif not args.protocol:
+    print("You did not supply a protocol to use, the default is ssh.")
+    connect_ssh(get_rndis_device())
+elif not args.username:
+    print("You need to supply a username")
+    sys.exit(1)
