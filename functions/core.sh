a4f74f8c3280d012e8015142567d5547eec62638
Alfred Neumayer <dev.beidl@gmail.com>
2018-08-30 04:08:48 +0200

    core: allow ext2 and 3 images to be mounted in addition to ext4

--- functions/core.sh
+++ functions/core.sh
@@ -15,7 +15,7 @@ function convert_rootfs() {
 }
 
 function convert_androidimage() {
-	if file $AND_IMAGE | grep "ext4 filesystem"; then
+	if file $AND_IMAGE | grep "ext[2-4] filesystem"; then
 		cp $AND_IMAGE $IMAGE_DIR/system.img
 	else
 		simg2img $AND_IMAGE $IMAGE_DIR/system.img
