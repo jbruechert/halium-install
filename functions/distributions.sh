fd8494d41a8026a17b1d037156f80382d766e8b5
doniks <p.putz@yahoo.de>
2019-02-18 19:20:33 +0100

    add -i flag which copies your personal ssh key into the image

--- functions/distributions.sh
+++ functions/distributions.sh
@@ -42,6 +42,31 @@ function chroot_run() {
 	sudo DEBIAN_FRONTEND=noninteractive LANG=C RUNLEVEL=1 chroot $ROOTFS_DIR /bin/bash -c "$@"
 }
 
+function copy_ssh_key_root() {
+	if $DO_COPY_SSH_KEY ; then
+		D=$ROOTFS_DIR/root/.ssh
+		echo "I: Copying ssh key to the user 'root'"
+
+		sudo mkdir $D
+		sudo tee -a $D/authorized_keys < $SSH_KEY >/dev/null
+		sudo chmod 0700 $D
+		sudo chmod 0600 $D/authorized_keys
+	fi
+}
+
+function copy_ssh_key_phablet() {
+	if $DO_COPY_SSH_KEY ; then
+		D=$ROOTFS_DIR/home/phablet/.ssh
+		echo "I: Copying ssh key to the user 'phablet'"
+
+		sudo mkdir $D
+		sudo tee -a $D/authorized_keys < $SSH_KEY >/dev/null
+		sudo chown -R 32011:32011 $D
+		sudo chmod 0700 $D
+		sudo chmod 0600 $D/authorized_keys
+	fi
+}
+
 function post_install() {
 	if [ "$1" == "none" ]; then
 		return
@@ -60,9 +85,11 @@ function post_install() {
 	case "$1" in
 	halium | debian-pm | reference)
 		setup_passwd root $ROOTPASSWORD
+		copy_ssh_key_root
 
 		if chroot_run "id -u phablet" >/dev/null 2>&1; then
 			setup_passwd phablet $USERPASSWORD
+			copy_ssh_key_phablet
 		fi
 
 		sudo rm -f $ROOTFS_DIR/etc/dropbear/dropbear_{dss,ecdsa,rsa}_host_key
@@ -70,9 +97,11 @@ function post_install() {
 		;;
 	debian-pm-caf)
 		setup_passwd root $ROOTPASSWORD
+		copy_ssh_key_root
 
 		if chroot_run "id -u phablet" >/dev/null 2>&1; then
 			setup_passwd phablet $USERPASSWORD
+			copy_ssh_key_phablet
 		fi
 
 		sudo rm -f $ROOTFS_DIR/etc/dropbear/dropbear_{dss,ecdsa,rsa}_host_key
@@ -85,7 +114,9 @@ function post_install() {
 		;;
 	pm | neon)
 		setup_passwd root $ROOTPASSWORD
+		copy_ssh_key_root
 		setup_passwd phablet $USERPASSWORD
+		copy_ssh_key_phablet
 
 		# cant source /etc/environment
 		# LD_LIBRARY_ ; QML2_IMPORT_ derps
@@ -105,6 +136,7 @@ function post_install() {
 		echo "[done]"
 
 		setup_passwd phablet $USERPASSWORD
+		copy_ssh_key_phablet
 
 		sudo mkdir -p $ROOTFS_DIR/android/firmware
 		sudo mkdir -p $ROOTFS_DIR/android/persist
